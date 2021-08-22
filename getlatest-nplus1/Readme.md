8683ef3b844a86cb98106f665eaf71cc6eb8912e
hogesako <hogesako@ikasako.com>
2021-08-22 15:30:56 +0900

    n+1検証

--- getlatest-nplus1/Readme.md
+++ getlatest-nplus1/Readme.md
@@ -5,8 +5,36 @@
 isucon11予選からisuとisu_conditionだけ利用。
 
 # 検証
-## N+1 with 最適なインデックス
 
+## N+1 インデックスなし
+### 結果
+ブラウザ単発で300msくらい。スループットは5RPSくらい
+
+### 内部ループの実行計画
+74msぐらいかかっている。フルスキャン部分とソート部分が重い
+```
+explain analyze SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = '6892a276-e299-b319-1876-6d7bbaa1e176' ORDER BY `timestamp` DESC LIMIT 1
+| -> Limit: 1 row(s)  (cost=1034.85 rows=1) (actual time=74.241..74.242 rows=1 loops=1)
+    -> Sort: isu_condition.`timestamp` DESC, limit input to 1 row(s) per chunk  (cost=1034.85 rows=9946) (actual time=74.163..74.163 rows=1 loops=1)
+        -> Filter: (isu_condition.jia_isu_uuid = '6892a276-e299-b319-1876-6d7bbaa1e176')  (cost=1034.85 rows=9946) (actual time=0.740..69.887 rows=1000 loops=1)
+            -> Table scan on isu_condition  (cost=1034.85 rows=9946) (actual time=0.582..46.746 rows=10000 loops=1)
+```
+
+
+
+## N+1 with 最適なインデックス
+### 結果
+ブラウザ単発で180msくらい。スループットは10RPSくらい
+
+### 内部ループの実行計画
+実績値は3msくらいインデックスだけで取れている
+```
+ -> Limit: 1 row(s)  (cost=220.75 rows=1) (actual time=3.001..3.003 rows=1 loops=1)
+    -> Index lookup on isu_condition using jia_isu_uuid_timestamp_idx (jia_isu_uuid='6892a276-e299-b319-1876-6d7bbaa1e176'), with index condition: (isu_condition.jia_isu_uuid = '6892a276-e299-b319-1876-6d7bbaa1e176')  (cost=220.75 rows=1000) (actual time=2.985..2.985 rows=1 loops=1)
+```
+
+```
+```
 ## window関数
 
 ## group by and max句でのサブクエリ
--- getlatest-nplus1/main.go
+++ getlatest-nplus1/main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"sort"
 	"strings"
 	"time"
 
@@ -140,7 +139,6 @@ func main() {
 
 	e.GET("/api/initialize", getInitialize)
 	e.GET("/api/isu", getIsuList)
-	e.GET("/api/trend", getTrend)
 
 	mySQLConnectionData = NewMySQLConnectionEnv()
 
@@ -150,7 +148,7 @@ func main() {
 		e.Logger.Fatalf("failed to connect db: %v", err)
 		return
 	}
-	db.SetMaxOpenConns(10)
+	db.SetMaxOpenConns(100)
 	defer db.Close()
 
 	serverPort := fmt.Sprintf(":%v", getEnv("SERVER_APP_PORT", "3000"))
@@ -260,87 +258,6 @@ func getIsuList(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, responseList)
-}
-
-// GET /api/trend
-// ISUの性格毎の最新のコンディション情報
-func getTrend(c echo.Context) error {
-	characterList := []Isu{}
-	err := db.Select(&characterList, "SELECT `character` FROM `isu` GROUP BY `character`")
-	if err != nil {
-		c.Logger().Errorf("db error: %v", err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
-
-	res := []TrendResponse{}
-
-	for _, character := range characterList {
-		isuList := []Isu{}
-		err = db.Select(&isuList,
-			"SELECT * FROM `isu` WHERE `character` = ?",
-			character.Character,
-		)
-		if err != nil {
-			c.Logger().Errorf("db error: %v", err)
-			return c.NoContent(http.StatusInternalServerError)
-		}
-
-		characterInfoIsuConditions := []*TrendCondition{}
-		characterWarningIsuConditions := []*TrendCondition{}
-		characterCriticalIsuConditions := []*TrendCondition{}
-		for _, isu := range isuList {
-			conditions := []IsuCondition{}
-			err = db.Select(&conditions,
-				"SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ? ORDER BY timestamp DESC",
-				isu.JIAIsuUUID,
-			)
-			if err != nil {
-				c.Logger().Errorf("db error: %v", err)
-				return c.NoContent(http.StatusInternalServerError)
-			}
-
-			if len(conditions) > 0 {
-				isuLastCondition := conditions[0]
-				conditionLevel, err := calculateConditionLevel(isuLastCondition.Condition)
-				if err != nil {
-					c.Logger().Error(err)
-					return c.NoContent(http.StatusInternalServerError)
-				}
-				trendCondition := TrendCondition{
-					ID:        isu.ID,
-					Timestamp: isuLastCondition.Timestamp.Unix(),
-				}
-				switch conditionLevel {
-				case "info":
-					characterInfoIsuConditions = append(characterInfoIsuConditions, &trendCondition)
-				case "warning":
-					characterWarningIsuConditions = append(characterWarningIsuConditions, &trendCondition)
-				case "critical":
-					characterCriticalIsuConditions = append(characterCriticalIsuConditions, &trendCondition)
-				}
-			}
-
-		}
-
-		sort.Slice(characterInfoIsuConditions, func(i, j int) bool {
-			return characterInfoIsuConditions[i].Timestamp > characterInfoIsuConditions[j].Timestamp
-		})
-		sort.Slice(characterWarningIsuConditions, func(i, j int) bool {
-			return characterWarningIsuConditions[i].Timestamp > characterWarningIsuConditions[j].Timestamp
-		})
-		sort.Slice(characterCriticalIsuConditions, func(i, j int) bool {
-			return characterCriticalIsuConditions[i].Timestamp > characterCriticalIsuConditions[j].Timestamp
-		})
-		res = append(res,
-			TrendResponse{
-				Character: character.Character,
-				Info:      characterInfoIsuConditions,
-				Warning:   characterWarningIsuConditions,
-				Critical:  characterCriticalIsuConditions,
-			})
-	}
-
-	return c.JSON(http.StatusOK, res)
 }
 
 // ISUのコンディションの文字列からコンディションレベルを計算
